d7dfb939e8bf81cc8c0f4d4dba875869450fc4e3
Phạm Gia Phúc <robertpham8822@gmail.com>
2026-04-19 14:46:24 +0700

    Create ui/styles.py with CSS styling variables for the application

---
+++ ui/styles.py
@@ -0,0 +1,22 @@
+# MAIN_WINDOW_STYLE
+main_window_style = '''
+    background-color: #282c34;
+    color: white;
+    font-family: Arial, sans-serif;
+    padding: 20px;
+'''
+
+# CREATE_HOTKEY_STYLE
+create_hotkey_style = '''
+    background-color: #4caf50;
+    color: white;
+    font-family: Arial, sans-serif;
+    border: none;
+    padding: 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 12px;
+'''
