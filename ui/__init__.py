f1427b8b5d6655ddaa63108fa2ebaf82632eed0b
Phạm Gia Phúc <robertpham8822@gmail.com>
2026-04-30 15:52:54 +0700

    Create ui/__init__.py to make ui a Python package

---
+++ ui/__init__.py
@@ -0,0 +1,6 @@
+# UI Package
+from .main_window import MainWindow
+from .create_hotkey import CreateHotkeyWindow
+from .styles import main_window_style, create_hotkey_style
+
+__all__ = ['MainWindow', 'CreateHotkeyWindow', 'main_window_style', 'create_hotkey_style']
